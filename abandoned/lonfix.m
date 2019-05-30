6ecca9f91799940131796230e87a86af5af114d3
Luke Davis <lukelbd@gmail.com>
2019-05-30 11:49:15 +0200

    Move to old folder

--- abandoned/lonfix.m
+++
@@ -1,8 +0,0 @@
-function [newlon] = lonfix(lon);
-% Fixes longitudes for display purposes. Trivial, but useful.
-%
-    lon = mod(lon,360); 
-    filt = lon>180;
-    lon(filt) = lon(filt)-360;
-    newlon = lon;
-end
